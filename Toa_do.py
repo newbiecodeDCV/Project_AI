5ac2dbfd0299c5d81337a5d95d8bd7567f057b05
newbiecodeDCV <nguyentrungtrieu080705@gmail.com>
2023-12-14 00:50:32 +0700

    init project

---
+++ Toa_do.py
@@ -0,0 +1,222 @@
+import json
+import networkx as nx
+import matplotlib.pyplot as plt
+import heapq
+import folium
+with open('export.geojson') as f:
+    geojson_data = json.load(f)
+
+# Trích xuất tọa độ
+coordinates = []
+
+for feature in geojson_data['features']:
+    geometry = feature['geometry']
+    if geometry['type'] == 'Point':
+        coordinates.append(geometry['coordinates'])
+
+# Sắp xếp tọa độ theo thứ tự
+sorted_coordinates = sorted(coordinates, key=lambda x: x[0])
+
+# Lưu trữ tọa độ vào dict với tên theo thứ tự 'point_1', 'point_2', ...
+coordinate_dict = {}
+
+for i, coord in enumerate(sorted_coordinates):
+    key = f'point_{i + 1}'
+    value = [coord[1],coord[0]]
+    coordinate_dict[key] = value
+
+# Danh sách các điểm mới
+new_point=[[21.01125,105.84794],
+           [21.01113,105.84725],
+           [21.01104,105.84675],
+           [21.01105,105.84664],
+           [21.01109,105.84654],
+           [21.0112,105.84653],
+           [21.01124,105.84639],
+           [21.01133,105.84593],
+           [21.01331,105.84829],
+           [21.01321,105.84743],
+           [21.01421,105.84832],
+           [21.01418,105.84763],
+           [21.01323,105.84702],
+           [21.01246,105.84716],
+           [21.01303,105.84828],
+           [21.01279,105.84821],
+           [21.01256,105.84876],
+           [21.01103,105.8509],
+           [21.01053,105.85085],
+           [21.00911,105.85062],
+           [21.00993,105.85022],
+           [21.00951,105.84986],
+           [21.00871,105.84985],
+           [21.0089,105.84598],
+           [21.00968,105.84562],
+           [21.01001,105.84559],
+           [21.01085,105.84582],
+           [21.01066,105.84606],
+           [21.01061,105.84649],
+           [21.01056,105.84611],
+           [21.0104,105.84639],
+           [21.01009,105.84628],
+           [21.01002,105.84594],
+           [21.00997,105.84595],
+           [21.00995,105.84623],
+           [21.0099,105.84623],
+           [21.00968,105.84624],
+           [21.00945,105.84626],
+           [21.00901,105.84656],
+           [21.00865,105.84681],
+           [21.00824,105.84698],
+           [21.00856,105.8475],
+           [21.00833,105.84754],
+           [21.00992,105.84674],
+           [21.00984,105.84737],
+           [21.00985,105.84777],
+           [21.00967,105.84776],
+           [21.00964,105.84751],
+           [21.00967,105.84711],
+           [21.00912,105.8477],
+           [21.00876,105.84772],
+           [21.01001,105.84779],
+           [21.01006,105.84732],
+           [21.01071,105.84787],
+           [21.01066,105.84829],
+           [21.01094,105.84837],
+           [21.01118,105.84857],
+           [21.01063,105.84848],
+           [21.00976,105.8484],
+           [21.00969,105.84834],
+           [21.00905,105.84825],
+           [21.00969,105.8486],
+           [21.00937,105.8486]
+
+           ]
+for i in range(len(new_point)) :
+    key =f'new_point_{i+1}'
+    coordinate_dict[key] = new_point[i]
+
+# Tạo danh sách các cạnh
+
+edge_list = [('point_14','point_12'),('point_12','point_14'),
+             ('point_11','point_12'),('point_12','point_11'),
+             ('point_10','point_11'),('point_11','point_10'),
+             ('point_10','point_9'),('point_9','point_10'),
+             ('point_9','point_8'),('point_8','point_9'),
+             ('point_8','point_7'),('point_7','point_8'),
+             ('point_7','point_6'),('point_6','point_7'),
+             ('point_6','new_point_13'),('new_point_13','point_6'),
+             ('new_point_10','new_point_13'),('new_point_13','new_point_10'),
+             ('new_point_10','new_point_12'),('new_point_12','new_point_10'),
+             ('new_point_10','new_point_9'),('new_point_9','new_point_10'),
+             ('new_point_11','new_point_9'),('new_point_9','new_point_11'),
+             ('new_point_9','point_23'),('point_23','new_point_9'),
+             ('point_24','point_23'),
+             ('point_6','point_5'),('point_5','point_6'),
+             ('point_5','new_point_14'),
+             ('new_point_10','new_point_14'),('new_point_14','new_point_10'),
+             ('new_point_14','point_5'),('point_5','new_point_14'),
+             ('point_21','point_15'),
+             ('new_point_15','new_point_9'),('new_point_9','new_point_15'),
+             ('new_point_15','new_point_16'),('new_point_16','new_point_15'),
+             ('new_point_17','new_point_16'),('new_point_16','new_point_17'),
+             ('new_point_17','point_22'),('point_22','new_point_17'),
+             ('point_23','point_22'),
+             ('point_22','point_21'),
+             ('point_21','point_20'),('point_20','point_19'),
+             ('point_16','point_20'),('point_20','point_16'),
+             ('point_15','new_point_57'),('new_point_57','point_15'),
+             ('point_16','new_point_57'),('new_point_57','point_16'),
+             ('point_26','point_21'),('point_25','point_26'),('point_26','point_25'),
+             ('point_19','point_25'),('point_25','point_28'),('point_28','new_point_19'),
+             ('new_point_19','new_point_18'),('new_point_18','new_point_19'),
+             ('point_27','point_19'),('point_19','point_27'),('point_27','new_point_20'),
+             ('new_point_20','new_point_19'),('new_point_19','new_point_20'),
+             ('point_4','point_5'),('point_5','point_4'),
+             ('point_4','point_3'),('point_3','point_4'),
+             ('point_3','point_1'),('point_1','point_3'),
+             ('point_1','new_point_8'),('new_point_8','point_1'),
+             ('new_point_27','new_point_8'),('new_point_8','new_point_27'),
+             ('new_point_27','new_point_26'),('new_point_26','new_point_27'),
+             ('new_point_25','new_point_26'),('new_point_26','new_point_25'),
+             ('new_point_25','new_point_24'),('new_point_24','new_point_25'),
+             ('new_point_24','point_2'),('point_2','new_point_24'),
+             ('new_point_23','point_18'),('point_18','point_13'),('point_13','new_point_43'),
+             ('new_point_43','new_point_41'),('new_point_41','point_2'),
+             ('new_point_22','point_27'),('point_27','new_point_22'),
+             ('new_point_22','new_point_23'),('new_point_23','new_point_22'),
+             ('point_19','point_17'),('point_17','point_18'),
+             ('point_15','new_point_1'),('new_point_1','point_15'),
+             ('new_point_54','new_point_1'),('new_point_1','new_point_54'),
+             ('new_point_54','new_point_52'),('new_point_52','new_point_54'),
+             ('new_point_52','new_point_46'),('new_point_46','new_point_47'),
+             ('new_point_50','new_point_47'),('new_point_47','new_point_50'),
+             ('new_point_50','new_point_51'),('new_point_51','new_point_50'),
+             ('new_point_51','point_13'),('point_13','new_point_51'),
+             ('new_point_57','new_point_56'),('new_point_56','new_point_57'),
+             ('new_point_56','new_point_55'),('new_point_55','new_point_56'),
+             ('new_point_54','new_point_55'),('new_point_55','new_point_58'),
+             ('new_point_58','new_point_59'),('new_point_59','new_point_60'),('new_point_60','new_point_61'),
+             ('new_point_61','new_point_50'),('new_point_59','new_point_62'),('new_point_62','new_point_59'),
+             ('new_point_62','point_17'),('point_17','new_point_62'),
+             ('new_point_62','new_point_63'),('new_point_63','new_point_62'),
+             ('new_point_1','new_point_2'),('new_point_2','new_point_1'),('new_point_2','new_point_3'),('new_point_3','new_point_2'),
+             ('new_point_3','new_point_4'),('new_point_4','new_point_3'),('new_point_4','new_point_5'),('new_point_5','new_point_4'),
+             ('new_point_5','new_point_6'),('new_point_6','new_point_5'),('new_point_6','new_point_7'),('new_point_7','new_point_6'),
+             ('new_point_7','new_point_8'),('new_point_8','new_point_7'),
+             ('new_point_27','new_point_28'),('new_point_28','new_point_27'),
+             ('new_point_28','new_point_29'),('new_point_29','new_point_28'),
+             ('new_point_28','new_point_30'),('new_point_30','new_point_28'),
+             ('new_point_30','new_point_31'),('new_point_31','new_point_30'),
+             ('new_point_33','new_point_30'),('new_point_30','new_point_33'),
+             ('new_point_33','new_point_34'),('new_point_34','new_point_33'),
+             ('new_point_34','new_point_36'),('new_point_36','new_point_34'),
+             ('new_point_31','new_point_32'),('new_point_32','new_point_31'),
+             ('new_point_31','new_point_32'),('new_point_32','new_point_31'),
+             ('new_point_32','new_point_35'),('new_point_35','new_point_32'),
+             ('new_point_35','new_point_36'),('new_point_36','new_point_35'),
+             ('new_point_36','new_point_37'),('new_point_37','new_point_36'),
+             ('new_point_37','new_point_38'),('new_point_38','new_point_37'),
+             ('new_point_38','new_point_39'),('new_point_39','new_point_38'),
+             ('new_point_39','new_point_40'),('new_point_40','new_point_39'),
+             ('new_point_40','new_point_41'),('new_point_41','new_point_40'),
+             ('new_point_42','new_point_43'),('new_point_43','new_point_42'),
+             ('new_point_32','new_point_44'),('new_point_44','new_point_32'),
+             ('new_point_44','new_point_45'),('new_point_45','new_point_44'),
+             ('new_point_45','new_point_46'),('new_point_46','new_point_45'),
+             ('new_point_52','new_point_53'),('new_point_53','new_point_52'),
+             ('new_point_47','new_point_48'),('new_point_48','new_point_47'),
+             ('new_point_48','new_point_49'),('new_point_49','new_point_48')
+]
+
+
+
+def euclidean_distance(coord1, coord2):
+    return ((coord1[0] - coord2[0]) ** 2 + (coord1[1] - coord2[1]) ** 2) ** 0.5
+
+
+def creat_graph(coordinate_dict,edge_list):
+    g = nx.DiGraph()
+    g.add_nodes_from(coordinate_dict.keys())
+    nx.set_node_attributes(g, coordinate_dict, 'pos')
+
+    for edge in edge_list:
+      node1, node2 = edge
+      weight = euclidean_distance(coordinate_dict[node1], coordinate_dict[node2])
+      g.add_edge(node1, node2, weight=weight)
+    return g
+
+g = creat_graph(coordinate_dict, edge_list)
+
+
+node = 'new_point_1'
+neighbors = g.neighbors(node)
+print(neighbors)
+
+
+
+
+
+
+
+
+
---
+++ app.py
@@ -0,0 +1,106 @@
+from queue import PriorityQueue
+import math
+from flask import Flask, render_template, request, jsonify
+from Toa_do import g
+app = Flask(__name__)
+
+class Node:
+    def __init__(self, position, cost, parent=None):
+        self.position = position
+        self.cost = cost
+        self.parent = parent
+
+    def __lt__(self, other):
+        return self.cost < other.cost
+
+def euclidean_distance(point1, point2):
+    return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+
+def heuristic(a, b):
+    return euclidean_distance(a, b)
+
+def find_nearest_graph_point(target_point, graph_nodes):
+    nearest_point = None
+    min_distance = float('inf')
+
+    for node in graph_nodes:
+        node_pos = graph_nodes[node]['pos']
+        distance = euclidean_distance(target_point, node_pos)
+
+        if distance < min_distance:
+            min_distance = distance
+            nearest_point = node_pos
+
+    return nearest_point
+
+def astar(start, end, graph):
+    open_set = PriorityQueue()
+    start_node = Node(start, 0)
+    open_set.put(start_node)
+
+    start_tuple = tuple(start)
+    end_tuple = tuple(end)
+
+    came_from = {start_tuple: None}
+    cost_so_far = {start_tuple: 0}
+
+    while not open_set.empty():
+        current_node = open_set.get()
+        current_position_tuple = tuple(current_node.position)
+
+        if current_position_tuple == end_tuple:
+            path = []
+            while current_node:
+                path.append(current_node.position)
+                current_node = came_from[current_node.position]
+
+            print("Path:", path[::-1])  # In ra đường đi khi thuật toán kết thúc
+            return path[::-1]
+        else:
+            if current_position_tuple not in graph:
+                continue  # Bỏ qua nếu node không tồn tại trong đồ thị
+
+            for neighbor in graph.neighbors(current_position_tuple):
+                neighbor_pos = find_nearest_graph_point(neighbor, graph.nodes())
+
+                new_cost = cost_so_far[current_position_tuple] + graph[current_position_tuple][neighbor_pos]['weight']
+                neighbor_tuple = tuple(neighbor_pos)
+
+                if neighbor_tuple not in cost_so_far or new_cost < cost_so_far[neighbor_tuple]:
+                    cost_so_far[neighbor_tuple] = new_cost
+                    priority = new_cost + heuristic(neighbor_tuple, end_tuple)
+                    neighbor_node = Node(neighbor_tuple, priority, parent=current_node)
+                    open_set.put(neighbor_node)
+                    came_from[neighbor_tuple] = current_node
+
+            # In ra các bước của đường đi trong quá trình thực hiện
+            print("Current Position:", current_position_tuple)
+            print("Open Set:", [node.position for node in open_set.queue])
+            print("Came From:", came_from)
+            print("Cost So Far:", cost_so_far)
+
+    return None
+
+
+# Giả định biến g đã được định nghĩa trước đó
+
+
+@app.route('/')
+def index():
+    return render_template('interactive_map.html')
+
+@app.route('/get_coordinates', methods=['POST'])
+def get_coordinates():
+    data = request.json
+    print('Received data from client:', data)
+    start = tuple(data['start'])
+    end = tuple(data['end'])
+
+    # Sử dụng đồ thị tạo từ NetworkX
+    start = find_nearest_graph_point(start, g.nodes())
+    end = find_nearest_graph_point(end, g.nodes())
+    path_coordinates = astar(start, end, g)
+    return jsonify({'path': path_coordinates})
+
+if __name__ == '__main__':
+    app.run(debug=False)
